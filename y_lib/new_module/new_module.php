19f9a988b6580d0627b8b8cd3d4822894544aac3
yyy <yyy@pure.local>
2017-07-11 15:18:34 +0800

    123

--- y_lib/new_module/new_module.php
+++ y_lib/new_module/new_module.php
@@ -12,6 +12,7 @@ class newModule {
     private $service_path = '../../Application/Common/Service/';
     private $model_path = '../../Application/Common/Model/';
     private $controller_path = '../../Application/Admin/Controller/';
+    private $view_path = '../../Application/Admin/View/';
     public function __construct($name, $desc = ''){
         $this->name = ucfirst($name);
         $this->desc = ucfirst($desc);
@@ -39,6 +40,24 @@ class newModule {
         file_put_contents($this->controller_path . 'Ant' . $this->name . 'Controller.class.php', $content);
     }
 
+    public function gain_view() {
+        $content = file_get_contents('view/add.html');
+        $content = str_replace('___name___', $this->name, $content);
+        $content = str_replace('___desc___', $this->desc, $content);
+        $content = str_replace('___time___', date('Y-m-d H:i:s'), $content);
+        $path = $this->view_path . 'Ant'.$this->name;
+        mkdir($path, 777);
+        file_put_contents($path . '/add.html', $content);
+
+        $content = file_get_contents('view/index.html');
+        $content = str_replace('___name___', $this->name, $content);
+        $content = str_replace('___desc___', $this->desc, $content);
+        $content = str_replace('___time___', date('Y-m-d H:i:s'), $content);
+        file_put_contents($path . '/index.html', $content);
+
+
+    }
+
     public function gain($names) {
         if (isset($names['service'])) {
             $this->gain_service();
@@ -53,6 +72,12 @@ class newModule {
             $this->gain_controller();
 
         }
+
+        if (isset($names['view'])) {
+            $this->gain_view();
+
+        }
+
         echo 'success';
         exit();
     }
@@ -60,20 +85,30 @@ class newModule {
     public function gain_power($names) {
         if (isset($names['service'])) {
 
-            chmod($this->service_path . $this->name . 'Service.class.php', 777);
+            //chmod($this->service_path . $this->name . 'Service.class.php', 777);
             echo 'sudo chmod 777 '.'Application/Common/Service/'. $this->name . 'Service.class.php '. "\n";
 
         }
         if (isset($names['model'])) {
 
-            chmod($this->model_path . 'Nf' . $this->name . 'Model.class.php', 777);
+           // chmod($this->model_path . 'Nf' . $this->name . 'Model.class.php', 777);
             echo 'sudo chmod 777 '.'Application/Common/Model/'. 'Nf' . $this->name . 'Model.class.php '. "\n";
         }
         if (isset($names['controller'])) {
 
-            chmod($this->controller_path . 'Ant' . $this->name . 'Controller.class.php', 777);
+            //chmod($this->controller_path . 'Ant' . $this->name . 'Controller.class.php', 777);
             echo 'sudo chmod 777 '.'Application/Admin/Controller/' . 'Ant' . $this->name . 'Controller.class.php '. "\n";
         }
+
+        if (isset($names['view'])) {
+            echo 'sudo chmod -R 777 '.'Application/Admin/View/' . 'Ant' . $this->name . "\n";
+
+            //chmod($this->view_path . 'Ant' . $this->name . '/add.html', 777);
+           // echo 'sudo chmod 777 '.'Application/Admin/View/' . 'Ant' . $this->name . '/add.html '. "\n";
+            //chmod($this->view_path . 'Ant' . $this->name . '/index.html', 777);
+            //echo 'sudo chmod 777 '.'Application/Admin/View/' . 'Ant' . $this->name . '/index.html '. "\n";
+        }
+
         echo 'success';
         exit();
     }
@@ -81,5 +116,5 @@ class newModule {
 
 
 $newModule = new newModule('property', '产品属性');
-//$newModule->gain(['model'=>1, 'controller'=>1, 'service'=>1]);
-$newModule->gain_power(['model'=>1, 'controller'=>1, 'service'=>1]);
+//$newModule->gain(['model'=>1, 'controller'=>1, 'service'=>1, 'view'=>1]);
+$newModule->gain_power(['view'=>1]);
