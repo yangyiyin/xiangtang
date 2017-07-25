eb59f021d052013b1c0dbcb234deb944081243b1
yyy <yyy@pure.local>
2017-07-25 15:17:37 +0800

    123

--- Application/Api/Lib/AppVersionIndex.class.php
+++ Application/Api/Lib/AppVersionIndex.class.php
@@ -20,6 +20,7 @@ class AppVersionIndex extends BaseSapi{
         if ($data) {
             $data['apk_url'] = 'http://' . $_SERVER['HTTP_HOST'] . $data['apk_url'];
         }
+        $data['force_upgrade'] = boolval(intval($data['force_upgrade'])));
         $data = convert_obj($data, 'app_name=appName,version_code=versionCode,version_name=versionName,apk_url=apkUrl,change_log=changeLog,update_tips=updateTips,force_upgrade=forceUpgrade');
 
         echo json_encode($data);
