dea016953eae92f1ff6947b74dc30caebd80b8af
yyy <yyy@pure.local>
2017-07-26 16:08:54 +0800

    123

---
+++ Application/Api/Lib/AppVersionIndex.class.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yyy
+ * Date: 17/4/25
+ * Time: 下午8:25
+ */
+namespace Api\Lib;
+use Common\Service;
+class AppVersionIndex extends BaseSapi{
+    protected $method = parent::API_METHOD_GET;
+    private $AdService;
+    public function init() {
+        $this->AppVersionService = Service\AppVersionService::get_instance();
+    }
+
+    public function excute() {
+
+        $data = $this->AppVersionService->get_current();
+        if ($data) {
+            $data['apk_url'] = 'http://' . $_SERVER['HTTP_HOST'] . $data['apk_url'];
+        }
+        $data['force_upgrade'] = boolval(intval($data['force_upgrade']));
+        $data = convert_obj($data, 'app_name=appName,version_code=versionCode,version_name=versionName,apk_url=apkUrl,change_log=changeLog,update_tips=updateTips,force_upgrade=forceUpgrade');
+
+        echo json_encode($data);
+        exit();
+    }
+
+}
