901b389e41b3f5d343b7257e60d4933ec74f5837
yyy <yyy@pure.local>
2017-08-01 21:16:59 +0800

    123

--- Application/Api/Lib/OrderPre_order_info.class.php
+++ Application/Api/Lib/OrderPre_order_info.class.php
@@ -18,64 +18,77 @@ class OrderPre_order_info extends BaseApi{
     }
 
     public function excute() {
-        $pre_order_id = I('get.pre_order_id');
-        $pre_order = $this->OrderPreService->get_info_by_id($pre_order_id);
-        if (!$pre_order) {
+        $pre_order_ids = I('get.pre_order_ids');
+        $pre_orders = $this->OrderPreService->get_by_ids($pre_order_ids);
+        if (!$pre_orders) {
             return result_json(FALSE, '没有预订单信息');
         }
+        $result = [];
+        foreach ($pre_orders as $pre_order) {
+            $pre_order_items = $this->OrderItemPreService->get_by_oid($pre_order['id']);
+            $ProductSkuService = \Common\Service\ProductSkuService::get_instance();
+            $sku_ids = result_to_array($pre_order_items, 'sku_id');
+            $skus = $ProductSkuService->get_by_ids($sku_ids);
+            $skus_map = result_to_map($skus);
+            $iids = result_to_array($pre_order_items, 'iid');
+            $ItemService = Service\ItemService::get_instance();
+            $items = $ItemService->get_by_ids($iids);
+            $items_map  = result_to_map($items);
 
-        $pre_order_items = $this->OrderItemPreService->get_by_oid($pre_order['id']);
+            //$item_num_map = result_to_map($pre_order_items, 'iid');
+            $items = $this->add_item_num($pre_order_items, $skus_map);
 
-        $iids = result_to_array($pre_order_items, 'iid');
-        $ItemService = Service\ItemService::get_instance();
-        $items = $ItemService->get_by_ids($iids);
-        $item_num_map = result_to_map($pre_order_items, 'iid');
-        $items = $this->add_item_num($items, $item_num_map);
+            $items = $this->convert_data($items, $items_map, $skus_map);
+            $data = [];
+            $data['pre_order_id'] = (int) $pre_order['id'];
+            $data['total_num'] = (int) $pre_order['num'];
+            $data['total_price'] = (int) $pre_order['sum'];
+            $data['item_list'] = $items;
+            $result[] = $data;
+        }
 
-        $items = $this->convert_data($items);
-        $data = [];
-        $data['pre_order_id'] = (int) $pre_order['id'];
-        $data['total_num'] = (int) $pre_order['num'];
-        $data['total_price'] = (int) $pre_order['sum'];
-        $data['item_list'] = $items;
 
-        return result_json(TRUE, '', $data);
+        return result_json(TRUE, '', $result);
     }
 
-    private function convert_data($data) {
+    private function convert_data($data, $items_map, $skus_map) {
         $list = [];
         if ($data) {
-            $itemUsertypePricesService = \Common\Service\ItemUsertypePricesService::get_instance();
-            $iids = result_to_array($data);
-            $prices = $itemUsertypePricesService->get_by_iids($iids);
-            $prices_map = result_to_complex_map($prices, 'iid');
             $UserService = Service\UserService::get_instance();
-            $user_info = $UserService->get_info_by_id($this->uid);
+            $user_info = $this->user_info;
             foreach ($data as $key => $_item) {
-                $_item['img'] = item_img(get_cover($_item['img'], 'path'));//todo 这种方式后期改掉
-                if (isset($prices_map[$_item['id']])) {
-                    $price = $itemUsertypePricesService->get_price_by_type($user_info['type'], $prices_map[$_item['id']]);
-                    if ($price) {
-                        $_item['price'] = $price;
-                    }
+
+                if ($UserService->is_dealer($user_info['type'])) {
+                    $_item['price'] = (int) $skus_map[$_item['sku_id']]['dealer_price'];
+                    $_item['show_price'] = (int) $skus_map[$_item['sku_id']]['price'];
+                    $_item['pay_price'] = (int) $skus_map[$_item['sku_id']]['dealer_price'];
+                } elseif ($UserService->is_normal($user_info['type'])) {
+                    $_item['price'] = (int) $skus_map[$_item['sku_id']]['price'];
+                    $_item['show_price'] = (int) $skus_map[$_item['sku_id']]['price'];
+                    $_item['pay_price'] = (int) $skus_map[$_item['sku_id']]['price'];
                 }
+
+                $_item['img'] = item_img(get_cover($items_map[$_item['item_id']]['img'], 'path'));//todo 这种方式后期改掉
                 $_item['num'] = (int)  $_item['num'];
-                $_item['id'] = (int) $_item['id'];
-                $_item['pid'] = (int) $_item['pid'];
-                $_item['price'] = (int) $_item['price'];
-                $list[] = convert_obj($_item, 'id=item_id,pid,title,img,desc,unit_desc,price,num');
+                $_item['id'] = (int) $items_map[$_item['item_id']]['id'];
+                $_item['pid'] = (int) $items_map[$_item['item_id']]['pid'];
+                $_item['price'] = (int) $items_map[$_item['item_id']]['price'];
+                $_item['title'] = $items_map[$_item['item_id']]['title'];
+                $_item['desc'] =  $items_map[$_item['item_id']]['desc'];
+                $_item['unit_desc'] = $items_map[$_item['item_id']]['unit_desc'];
+                $list[] = convert_obj($_item, 'id=item_id,pid,title,img,desc,unit_desc,price,num,show_price,pay_price');
             }
 
         }
         return $list;
     }
 
-    private function add_item_num($data, $item_num_map) {
+    private function add_item_num($data, $sku_map) {
         $list = [];
         if ($data) {
             foreach ($data as $key => $_item) {
-                if (isset($item_num_map[$_item['id']]['num'])) {
-                    $_item['num'] = (int) $item_num_map[$_item['id']]['num'];
+                if (isset($sku_map[$_item['sku_id']]['num'])) {
+                    $_item['num'] = (int) $sku_map[$_item['sku_id']]['num'];
                 }
                 $list[] = $_item;
             }
