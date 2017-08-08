1d5e41717c563b7e3624c5c12902082e63268593
yyy <yyy@pure.local>
2017-08-08 21:16:48 +0800

    123

---
+++ Application/Common/Model/NfOrderBenefitModel.class.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by newModule.
+ * Time: 2017-08-08 21:16:23
+ */
+namespace Common\Model;
+use Think\Model;
+class NfOrderBenefitModel extends NfBaseModel {
+  protected $_validate = array(
+        /**
+        array('title', 'require', '名称不能为空', self::EXISTS_VALIDATE, 'regex', self::MODEL_INSERT),
+        array('cid', 'require', '分类不能为空', self::EXISTS_VALIDATE, 'regex', self::MODEL_INSERT),
+        array('img', 'require', '图片不能为空', self::EXISTS_VALIDATE, 'regex', self::MODEL_INSERT),
+        array('unit_desc', 'require', '单位不能为空', self::EXISTS_VALIDATE, 'regex', self::MODEL_INSERT)
+        */
+    );
+}
