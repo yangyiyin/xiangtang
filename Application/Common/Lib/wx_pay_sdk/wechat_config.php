7f67f1113bf474e57ac21f496019309e70f5c326
yyy <yyy@pure.local>
2017-09-19 13:21:52 +0800

    123

--- Application/Common/Lib/wx_pay_sdk/wechat_config.php
+++ Application/Common/Lib/wx_pay_sdk/wechat_config.php
@@ -1,11 +1,11 @@
 <?php
-$wechat_config['APPID'] = "wx645ef1133169a3da";// 公众号身份标识
+$wechat_config['APPID'] = "wxf716bd5d6844abf4";//
 
 $wechat_config['APPSECRET'] = "";// 权限获取所需密钥 Key
 
 $wechat_config['KEY'] = "";// 加密密钥 Key，也即appKey
 
-$wechat_config['MCH_ID'] = '1467948202';// 财付通商户身份标识
+$wechat_config['MCH_ID'] = '1488335702';// 财付通商户身份标识
 
 $wechat_config['NOTIFY_URL'] = 'http://118.178.224.208/index.php/API/Pay/wechat_pay_notify.php';// 微信支付完成服务器通知页面地址
 
