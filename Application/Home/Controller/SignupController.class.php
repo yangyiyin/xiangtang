40e80fbdd6222855d2a1018ba33f130dc9e535a6
yyy <yyy@pure.local>
2017-09-22 21:57:30 +0800

    123

---
+++ Application/Home/Controller/SignupController.class.php
@@ -0,0 +1,25 @@
+<?php
+
+// +----------------------------------------------------------------------
+// | Author: Jroy 
+// +----------------------------------------------------------------------
+
+namespace Home\Controller;
+use Think\Controller;
+
+/**
+ * 前台首页控制器
+ * 主要获取首页聚合数据
+ */
+class SignupController extends Controller {
+
+	//系统首页
+    public function index(){
+
+
+
+                 
+        $this->display();
+    }
+
+}
