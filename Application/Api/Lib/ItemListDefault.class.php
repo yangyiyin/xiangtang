a9420f52a73d833f6a2ffb3d41cb95396a1dada9
yyy <178340042@qq.com>
2017-10-31 16:22:07 +0800

    123

--- Application/Api/Lib/ItemListDefault.class.php
+++ Application/Api/Lib/ItemListDefault.class.php
@@ -62,7 +62,7 @@ class ItemListDefault extends BaseSapi{
                 $_item['id'] = (int) $_item['id'];
                 $_item['pid'] = (int) $_item['pid'];
                 $_item['price'] = (int) $_item['price'];
-                $_item['show_price'] = (int) $_item['show_price'];
+                $_item['show_price'] = (int) $_item['price'];
                 $_item['sold_num'] = (int) $_item['sold_num'];
                 $list[] = convert_obj($_item, 'id=item_id,pid,title,img,desc,unit_desc,price,sold_num');
             }
