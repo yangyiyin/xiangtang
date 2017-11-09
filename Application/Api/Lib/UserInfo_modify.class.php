d228f591d764ef7dc0c850d92ddd4e36a71623ed
yyy <178340042@qq.com>
2017-11-09 11:40:41 +0800

    123

--- Application/Api/Lib/UserInfo_modify.class.php
+++ Application/Api/Lib/UserInfo_modify.class.php
@@ -38,4 +38,31 @@ class UserInfo_modify extends BaseApi{
         }
         result_json(TRUE, '修改成功!');
     }
+
+    public function uploadPicture(){
+
+        /* 返回标准数据 */
+        $return  = array('status' => 1, 'info' => '上传成功', 'data' => '');
+
+        /* 调用文件上传组件上传文件 */
+        $Picture = D('Picture');
+        $pic_driver = C('PICTURE_UPLOAD_DRIVER');
+        $info = $Picture->upload(
+            $_FILES,
+            C('PICTURE_UPLOAD'),
+            C('PICTURE_UPLOAD_DRIVER'),
+            C("UPLOAD_{$pic_driver}_CONFIG")
+        ); //TODO:上传到远程服务器
+
+        /* 记录图片信息 */
+        if($info){
+            $return['status'] = 1;
+            $return = array_merge($info['download'], $return);
+        } else {
+            $return['status'] = 0;
+            $return['info']   = $Picture->getError();
+        }
+
+        $this->ajaxReturn($return);
+    }
 }
