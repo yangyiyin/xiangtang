36c4e3bc2c14593f61fd2ad7c82289bb76e6befb
yyy <178340042@qq.com>
2017-12-20 15:01:56 +0800

    123

--- Application/Api/Lib/PromotionCouponList.class.php
+++ Application/Api/Lib/PromotionCouponList.class.php
@@ -22,16 +22,18 @@ class PromotionCouponList extends BaseApi{
 
 
         if ($my_coupons) {
+            $new_my_coupons = [];
             foreach ($my_coupons as $key => $coupon) {
                 if ($sum < $coupon['least']) {
                     continue;
                 }
                 $my_coupons[$key]['function'] = 'function (data){data["new_total_price"] = data["new_total_price"]?data["new_total_price"]:data["total_price"];if(data["new_total_price"]>='.$coupon['least'].'){data["new_total_price"]-='.$coupon['deductible'].'; data["coupon_discount"] = data["coupon_discount"]?data["coupon_discount"]:0;data["coupon_discount"]+='.$coupon['deductible'].'}';
                 $my_coupons[$key]['desc'] = '满'.$coupon['least'].'可用';
+                $new_my_coupons[] = $my_coupons[$key];
             }
         }
-        $my_coupons = $my_coupons ? $my_coupons : [];
-        $my_coupons = convert_objs($my_coupons, 'id=coupon_id,code,title,desc,least,deductible,function');
+        $my_coupons = $new_my_coupons ? $new_my_coupons : [];
+        $my_coupons = convert_objs($new_my_coupons, 'id=coupon_id,code,title,desc,least,deductible,function');
         return result_json(TRUE, '', $my_coupons);
     }
 
