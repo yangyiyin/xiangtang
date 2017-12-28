1053e6d1195fc6f6c0c91606c3f77784fcc60ba8
yyy <178340042@qq.com>
2017-12-28 10:50:38 +0800

    123

--- Application/Api/Lib/PromotionCouponTake.class.php
+++ Application/Api/Lib/PromotionCouponTake.class.php
@@ -15,8 +15,8 @@ class PromotionCouponTake extends BaseApi{
 
     public function excute() {
         $id = $this->post_data['id'];
-
-        $info = $this->DeductibleCouponService->get_info_by_id($id);
+        $DeductibleCouponService = \Common\Service\DeductibleCouponService::get_instance();
+        $info = $DeductibleCouponService->get_info_by_id($id);
         if (!$info) {
             $this->error('没有对应的优惠券信息');
         }
