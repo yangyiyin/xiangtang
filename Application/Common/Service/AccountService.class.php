db1042cd7cd2738564ffd8d914aca803ddf02e3c
yyy <178340042@qq.com>
2018-01-08 17:18:50 +0800

    324

--- Application/Common/Service/AccountService.class.php
+++ Application/Common/Service/AccountService.class.php
@@ -127,6 +127,7 @@ class AccountService extends BaseService{
     }
 
     public function minus_account($uid, $sum) {
+        
         if (!$uid || !$sum) {
             return result(FALSE, '参数错误');
         }
