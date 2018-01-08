f61e94da667bb3f34fccc11290826e877e387155
yyy2 <178340045@qq.com>
2018-01-08 16:53:30 +0800

    123

--- mysql.sql
+++ mysql.sql
@@ -15,7 +15,6 @@ CREATE TABLE `shopy_nf_account` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
-
 CREATE TABLE `shopy_nf_order_benefit` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   
