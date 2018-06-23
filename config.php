3cfcc9c878bf0854ca0aaaeebc54e657198f2982
yyy <178340042@qq.com>
2018-06-23 14:33:22 +0800

    123

--- config.php
+++ config.php
@@ -37,7 +37,7 @@ return array(
     /* 数据库配置 */
     'DB_TYPE'   => 'mysql', // 数据库类型
     'DB_HOST'   => '127.0.0.1', // 服务器地址
-//    'DB_NAME'   => 'public', // 数据库名
+    'DB_NAME'   => 'managerrecommend', // 数据库名
 //    'DB_USER'   => 'gd_ant_user', // 用户名
 //    'DB_PWD'    => 'fpf5Vsdv2HfANSAB',  // 密码
     'DB_USER'   => 'root', // 用户名
