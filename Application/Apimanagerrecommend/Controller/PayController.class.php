77f01bc728c172c06fb0a231f11ab1843b8ead59
yyy <178340042@qq.com>
2018-07-11 08:58:50 +0800

    123

--- Application/Apimanagerrecommend/Controller/PayController.class.php
+++ Application/Apimanagerrecommend/Controller/PayController.class.php
@@ -8,17 +8,17 @@
 namespace Apimanagerrecommend\Controller;
 class PayController extends BaseController {
     public function alipay_create() {
-        $this->excute_api('Api\Lib\AlipayCreate');
+        $this->excute_api('Apimanagerrecommend\Lib\AlipayCreate');
     }
 
     public function alipay_notify() {
-        $this->excute_api('Api\Lib\AlipayNotify');
+        $this->excute_api('Apimanagerrecommend\Lib\AlipayNotify');
     }
 
     public function wechat_pay_create() {
-        $this->excute_api('Api\Lib\WechatPayCreate');
+        $this->excute_api('Apimanagerrecommend\Lib\WechatPayCreate');
     }
     public function wechat_pay_notify() {
-        $this->excute_api('Api\Lib\WechatPayNotify');
+        $this->excute_api('Apimanagerrecommend\Lib\WechatPayNotify');
     }
 }
