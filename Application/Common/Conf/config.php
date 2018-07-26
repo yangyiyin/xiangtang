834077a05ef5f1ef824e466375cd688d7f17d266
yyy <178340042@qq.com>
2018-07-26 15:35:21 +0800

    123

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -8,7 +8,7 @@
  * 系统配文件
  * 所有系统级别的配置
  */
-return array(
+$arr = array(
     /* 模块相关配置 */
     'AUTOLOAD_NAMESPACE' => array('Addons' => ZCMS_ADDON_PATH), //扩展模块列表
     'DEFAULT_MODULE'     => 'Home',
@@ -56,5 +56,14 @@ return array(
 
     'GROUP_FRANCHISEE' => 4,
     'SERVER_CIDS' => [8,19],
-    'INVITER_RATE' => 0.01
+    'INVITER_RATE' => 0.01,
+    'BASE_WEB_HOST' => '88plus.net'
 );
+
+if (file_exists(__ROOT__.'/product.lock')) {
+    $arr['DB_HOST'] = '127.0.0.1';
+    $arr['DB_PWD'] = 'a123123A';
+    $arr['BASE_WEB_HOST'] = 'yixsu.com';
+}
+
+return $arr;
