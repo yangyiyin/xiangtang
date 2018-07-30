13279f31fcc5a34616af8c06c8ff443215650ed5
yyy <178340042@qq.com>
2018-07-30 15:15:48 +0800

    123

--- Application/Apimanagerrecommend/Lib/LaughSysTips.class.php
+++ Application/Apimanagerrecommend/Lib/LaughSysTips.class.php
@@ -16,15 +16,18 @@ class LaughSysTips extends BaseSapi{
 
     public function excute() {
         $infos = [];
-        $infos[]= '店长小助手:选择活动类别进行活动页面制作!';
-        $infos[]= '店长小助手:在模板库可以选择自己喜欢的模板进行添加,然后制作!';
-        $infos[]= '店长小助手:可以在个人中心点击头像设置个性化头像哦';
-        $infos[]= '店长小助手:长按模板图片可以删除哦';
-        $infos[]= '店长小助手:制作完的页面,可以到个人中心我制作的页面里面查看';
-        $infos[]= '店长小助手:长按我的页面可以删除操作';
+        $infos[]= ['title'=>'店长小助手:选择活动类别进行活动页面制作!', 'link'=>''];
+        $infos[]= ['title'=>'店长小助手:在模板库可以选择自己喜欢的模板进行添加!', 'link'=>''];
+        $infos[]= ['title'=>'店长小助手:可以在个人中心点击头像设置个性化头像哦!', 'link'=>''];
+        $infos[]= ['title'=>'店长小助手:长按模板图片可以删除哦!', 'link'=>''];
+//        $infos[]= '店长小助手:在模板库可以选择自己喜欢的模板进行添加,然后制作!';
+//        $infos[]= '店长小助手:可以在个人中心点击头像设置个性化头像哦';
+//        $infos[]= '店长小助手:长按模板图片可以删除哦';
+//        $infos[]= '店长小助手:制作完的页面,可以到个人中心我制作的页面里面查看';
+//        $infos[]= '店长小助手:长按我的页面可以删除操作';
 
-        $info = $infos[array_rand($infos,1)];
-        return result_json(TRUE, '', $info);
+//        $info = $infos[array_rand($infos,1)];
+        return result_json(TRUE, '', $infos);
     }
 
 }
